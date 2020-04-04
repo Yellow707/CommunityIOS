850cfa6f9ee3a8acc7da8b776417200417d8aff7
Igor Petrov <fox0234@yandex.ru>
2020-04-04 20:07:42 +0300

    create empty project

---
+++ Community/Community/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Community
+//
+//  Created by Igor Petrov on 04.04.2020.
+//  Copyright © 2020 ru.petrovid. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Community/Community/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Community
+//
+//  Created by Igor Petrov on 04.04.2020.
+//  Copyright © 2020 ru.petrovid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        print("viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
